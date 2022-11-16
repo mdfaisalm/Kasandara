ece01dc4046d96430f3ffd20008b8e2eb8c33bcb
FRandyDevs <faisalchanglah@gmail.com>
2022-11-16 22:39:28 +0500

    16Nov22 Class 2

---
+++ app/src/main/java/com/kassandra/apps/flutter/course/ConstraintLayoutExampleActivity.java
@@ -0,0 +1,14 @@
+package com.kassandra.apps.flutter.course;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConstraintLayoutExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraily_layout_example);
+    }
+}
