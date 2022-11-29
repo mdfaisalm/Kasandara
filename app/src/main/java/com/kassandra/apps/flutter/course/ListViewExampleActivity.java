2e38f7347f9a8686acea2b89e62c49e9935a02c2
FRandyDevs <faisalchanglah@gmail.com>
2022-11-29 09:15:30 +0500

    29Nov22

--- app/src/main/java/com/kassandra/apps/flutter/course/ListViewExampleActivity.java
+++ app/src/main/java/com/kassandra/apps/flutter/course/ListViewExampleActivity.java
@@ -3,7 +3,17 @@ package com.kassandra.apps.flutter.course;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class ListViewExampleActivity extends AppCompatActivity {
 
@@ -13,20 +23,83 @@ public class ListViewExampleActivity extends AppCompatActivity {
     String[] studentNamesArray = new String[]{"abc", "xyz", "studentName3", "studentName4"};
     int[] studentRollNosArray = new int[]{1, 2, 3, 4};
 
-    TextView tvStudentNames;
+    ArrayList<String> studentNamesArrayList = new ArrayList<>();
+    ArrayList<Integer> studentImagesArrayList = new ArrayList<>();
+
+    ListView lvStudentsNames;
+    ArrayAdapter<String> arrayAdapter;
+    CustomAdapter customAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
 
-        tvStudentNames = findViewById(R.id.tvStudentNames);
+        lvStudentsNames = findViewById(R.id.lvStudentsNames);
 
-//        tvStudentNames.setText(studentName + secondStudentName);
+        studentNamesArray = new String[]{"abc", "xyz", "studentName3"};
+        studentNamesArray = new String[]{"abc", "xyz", "studentName4", "studentName5"};
 
-        tvStudentNames.setText(studentRollNosArray[0] + " " + studentNamesArray[0] + "\n"
-                + studentRollNosArray[1] + " " + studentNamesArray[1] + "\n"
-                + studentRollNosArray[2] + " " + studentNamesArray[2] + "\n"
-                + studentRollNosArray[3] + " " + studentNamesArray[3]);
+        studentNamesArrayList.add("abc");//0>removed
+        studentNamesArrayList.add("xyz");//1>0>1>2>removed
+        studentNamesArrayList.remove("abc");//0?removed
+        studentNamesArrayList.add(0, "aaaa");//0
+        studentNamesArrayList.add("ccc");//2>3>2
+        studentNamesArrayList.add(1, "bbb");//1
+        studentNamesArrayList.remove(2);
+
+        studentImagesArrayList.add(R.drawable.bg_login_et);
+        studentImagesArrayList.add(R.drawable.download);
+        studentImagesArrayList.add(R.drawable.ic_launcher_background);
+
+
+        /*arrayAdapter = new ArrayAdapter<String>(this, R.layout.custom_lv_item, studentNamesArrayList);
+        lvStudentsNames.setAdapter(arrayAdapter);*/
+
+        customAdapter = new CustomAdapter();
+        lvStudentsNames.setAdapter(customAdapter);
+    }
+
+    public void addItem(View view) {
+        int size = studentNamesArrayList.size() + 1;
+        studentNamesArrayList.add("new Item " + size);
+        studentImagesArrayList.add(R.mipmap.ic_launcher);
+        customAdapter.notifyDataSetChanged();
+    }
+
+    class CustomAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return studentNamesArrayList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater layoutInflater = LayoutInflater.from(ListViewExampleActivity.this);
+            View view = layoutInflater.inflate(R.layout.custom_lv_item, null);
+            TextView tvTitle = view.findViewById(R.id.tvTitle);
+            ImageView ivLogo = view.findViewById(R.id.ivLogo);
+            String data = studentNamesArrayList.get(position);
+            ivLogo.setImageResource(studentImagesArrayList.get(position));
+            tvTitle.setText(data);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(ListViewExampleActivity.this, "Position=" + position, Toast.LENGTH_SHORT).show();
+                }
+            });
+            return view;
+        }
     }
 }
